29bc4396876132a5f2ffc7a63247c7c01a4d07de
Vaibhav Dhunde <dhybhav@gmail.com>
2025-03-23 15:48:28 +0530

    Register and redux setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
-import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Login from './components/Login';
 import Register from './components/Register';
 
 function App() {
   return (
-      <div className="App">
-        <Router>
+    <div className="App">
+      <Router>
         <Routes>
           {/* <Route path="/" element={<Home />} /> */}
-          <Route path="/login" element={<Login/>} />
+          <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
-        </Router>
-      </div>
+      </Router>
+    </div>
   )
 }
 
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,11 +1,65 @@
-import React from 'react';
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Row from "react-bootstrap/Row";
 
-const Register =()=>{
-    return(
-            <div>
-                <h1>Register</h1>
-            </div>
-        )
-}
+const Register = () => {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+  return (
+    <div
+      className="d-flex justify-content-center align-items-center"
+      style={{ height: "100vh" }}
+    >
+      <div className="card" style={{ width: "20rem" }}>
+        <div className="card-body">
+          <h1 className="mb-3">Register</h1>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Row className="mb-3">
+              <Form.Group as={Col} md="12" controlId="validationCustom03">
+                <Form.Control type="text" placeholder="Email" required />
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid city.
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Row>
+            <Row className="mb-3">
+              <Form.Group as={Col} md="12" controlId="validationCustom04">
+                <Form.Control type="text" placeholder="Password" required />
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid Password.
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Row>
+            <Row className="mb-3">
+              <Form.Group as={Col} md="12" controlId="validationCustom05">
+                <Form.Control
+                  type="text"
+                  placeholder="Confirm Password"
+                  required
+                />
+                <Form.Control.Feedback type="invalid">
+                  Passwords doesn't match.
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Row>
+            <Button type="submit">Submit form</Button>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Register;
