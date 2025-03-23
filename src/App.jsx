2cd22ecf1743dacbea413a9a67b67690cea68900
Rangasai Sandilya <saisandy97@gmail.com>
2025-03-23 14:37:21 +0530

    added bootstrap file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-       entry file
+       <div className="App">
+          main file 
+    </div>
     </>
   )
 }
