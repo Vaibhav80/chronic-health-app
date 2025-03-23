7b6a934beaa278578652c2e68697b51f89392a03
Rangasai Sandilya <saisandy97@gmail.com>
2025-03-23 17:00:30 +0530

    included popups for logout actions

--- src/components/SideNavbar.jsx
+++ src/components/SideNavbar.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import Swal from "sweetalert2";
 
 const SideNavbar =()=>{
     const navigate= useNavigate();
 
     const onLogout =()=>{
           // navigate back to login once loggged out
-          navigate('/login');
+          Swal.fire({
+            title: "Are you sure?",
+            text: "You will be logged out!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#d33",
+            cancelButtonColor: "#3085d6",
+            confirmButtonText: "Yes, logout!",
+          }).then((result) => {
+            if (result.isConfirmed) {
+                navigate('/login');
+            }
+          });
     }
     return (
         <div className="bg-dark text-white p-3 vh-100" style={{ width: "250px" }}>
