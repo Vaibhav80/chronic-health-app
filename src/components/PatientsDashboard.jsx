dee0090c14f0cd1d3412c4ad57f7ccd0d39bf20b
Vaibhav Dhunde <dhybhav@gmail.com>
2025-03-23 17:50:52 +0530

    added sidebar to  patients dashboard

--- src/components/PatientsDashboard.jsx
+++ src/components/PatientsDashboard.jsx
@@ -1,35 +1,42 @@
 import React from "react";
 import { Table } from "react-bootstrap";
 import { useGetPatientQuery } from "../../store/apiSlice";
+import SideNavbar from "./SideNavbar";
 
 const PatientsDashboard = () => {
   const { data: patients, isFetching } = useGetPatientQuery();
 
-  if (isFetching) {
-    return <p>Loading</p>;
-  }
-
   return (
-    <div class="d-flex flex-column pl-2 pr-2 pt-3">
-      <h4>Patients</h4>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Email</th>
-            <th>Blood Sugar</th>
-            <th>Body Temperature</th>
-          </tr>
-        </thead>
-        <tbody>
-          {patients.map((item) => (
-            <tr key={item.id}>
-              <td>{item.email}</td>
-              <td>{item.bloodSugar}</td>
-              <td>{item.temperature}</td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
+    <div className="d-flex">
+      <SideNavbar />
+
+      <div className="container-fluid p-4">
+        <div className="d-flex flex-column pl-2 pr-2 pt-3">
+          <h4>Patients</h4>
+          {isFetching ? (
+            <span>Loading...</span>
+          ) : (
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Email</th>
+                  <th>Blood Sugar</th>
+                  <th>Body Temperature</th>
+                </tr>
+              </thead>
+              <tbody>
+                {patients.map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.email}</td>
+                    <td>{item.bloodSugar}</td>
+                    <td>{item.temperature}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
